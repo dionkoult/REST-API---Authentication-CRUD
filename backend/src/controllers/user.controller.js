0fbfd1fc2ea8ab246351d4ba2a83d78608494db2
Dionusis Georgantopoulos <dionusiskoultouras@gmail.com>
2026-07-13 15:48:27 +0300

    Make login feature

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -44,6 +44,43 @@ const registerUser = async (req, res) => {
   }
 }
 
+const loginUser = async (req, res) => {
+  try {
+    // check if the user already exists
+    const { email, password } = req.body;
+
+    const user = await User.findOne({
+      email: email.toLowerCase()
+    });
+
+    if (!user) return res.status(400).json({
+      message: "User not found"
+    });
+
+    
+    // compare passwords
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) return res.status(400).json({
+      message: "Invalid credentials"
+    });
+
+    res.status(200).json({
+      message: "User Logged in",
+      user: {
+        id: user._id,
+        email: user.email,
+        username: user.username
+      }
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error"
+    });
+  }
+}
+
 export {
-  registerUser
+  registerUser,
+  loginUser
 }
